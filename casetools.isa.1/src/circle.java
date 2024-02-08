2b45a9851bdde906ab1495015c4b479e73a2058f
txuseef <tauseef.shaikh8625@gmail.com>
2024-02-08 10:25:46 +0530

    comit

---
+++ casetools.isa.1/src/circle.java
@@ -0,0 +1,16 @@
+
+public class circle {
+	
+	private double radius;
+	public circle(double radius) {
+		this.radius=radius;
+		
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius=radius;
+	}
+
+}
